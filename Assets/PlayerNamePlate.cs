6196ef1fba8eb532a250aad395d223cb1952c6dd
PapaFouras <nicolasg94@hotmail.fr>
2021-07-30 17:44:37 +0200

    EP 24

---
+++ Assets/PlayerNamePlate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNamePlate : MonoBehaviour
+{
+    [SerializeField]
+    Text usernameText;
+    [SerializeField]
+    private RectTransform healthBarFill;
+    [SerializeField]
+    private Player player;
+  
+    void Update()
+    {
+        usernameText.text = player.username;
+        healthBarFill.localScale = new Vector3(player.GetHealthPct(),1f,1f);
+    }
+}
